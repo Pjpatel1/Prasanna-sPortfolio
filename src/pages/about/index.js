b91fb023975f59b824b3e340180288e224e5da74
Prasannakumar <prasannakumar@Prasannakumars-Mac-mini.local>
2025-07-20 22:25:25 -0400

    Updated 20 July 2025 11:11

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -81,7 +81,7 @@ export const About = () => {
         </div>
            <div className="BodyText" ref={section1Ref}>
             <h2>Who am I</h2>
-            <h4>I am a Cybersecurity Analyst with 3 years of combined experience in software development and cybersecurity, holding a Master’s in Electrical and Computer Engineering with a focus on cybersecurity and a CompTIA Security+ certification. Proficient in SIEM tools like Wazuh, ELK Stack, and Splunk, as well as Metasploit, Kali Linux, and Ubuntu Server, I excel at automating security processes using Python and Shell scripting. My hands-on experience includes building a fully functional home lab to simulate and mitigate real-world threats, driven by a passion for protecting sensitive data and creating secure systems. Known for my creativity and problem-solving skills, I aim to refine and innovate cybersecurity practices, with a long-term goal of becoming a CISO. Let’s connect and build a safer digital future together.</h4>
+            <h4>Full-stack developer with 3+ years building scalable applications (Python, Java, React) and a Security + Certified, specializing in secure SDLC, cloud security (AWS/Azure), and threat mitigation. Passionate about developing robust software fortified against emerging threats.</h4>
           </div>
           <div className="BodyText">
             <h2>Education</h2>
